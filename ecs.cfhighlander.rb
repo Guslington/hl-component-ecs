912b2bfd0a4245399603199e5a0a5eebe493cfd0
Guslington <guslington@gmail.com>
2018-12-12 17:26:17 +1100

    extend ecs template from asg

--- ecs.cfhighlander.rb
+++ ecs.cfhighlander.rb
@@ -1,12 +1,10 @@
 CfhighlanderTemplate do
-  DependsOn 'vpc@1.2.0'
+  Extends 'asg@1.0.0'
+
   Parameters do
     ComponentParam 'EnvironmentName', 'dev', isGlobal: true
     ComponentParam 'EnvironmentType', 'development', isGlobal: true
     ComponentParam 'FileSystem' if enable_efs
-
   end
-
-  Component template: 'asg@1.0.0', name: 'asg', render: Inline
 
 end
--- ecs.cfndsl.rb
+++ ecs.cfndsl.rb
@@ -2,6 +2,17 @@ CloudFormation do
 
   Description "#{component_name} - #{component_version}"
 
+  if enable_efs
+    user_data << "mkdir /efs\n"
+    user_data << "yum install -y nfs-utils\n"
+    user_data << "mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${FileSystem}.efs.${AWS::Region}.amazonaws.com:/ /efs\n"
+  end
+
+  ecs_agent_extra_config.each do |key,value|
+    user_data << "#{key}=#{value} >> /etc/ecs/ecs.config\n"
+  end if defined? ecs_agent_extra_config
+
+
   ECS_Cluster('EcsCluster') {
     ClusterName FnSub("${EnvironmentName}-#{cluster_name}") if defined? cluster_name
   }
