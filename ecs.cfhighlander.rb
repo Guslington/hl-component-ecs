a03553e714005e6349047956faa8015a5909c4b0
Guslington <guslington@gmail.com>
2018-12-12 17:05:27 +1100

    inline asg component

--- ecs.cfhighlander.rb
+++ ecs.cfhighlander.rb
@@ -3,38 +3,10 @@ CfhighlanderTemplate do
   Parameters do
     ComponentParam 'EnvironmentName', 'dev', isGlobal: true
     ComponentParam 'EnvironmentType', 'development', isGlobal: true
-    ComponentParam 'Ami', type: 'AWS::EC2::Image::Id'
-    ComponentParam 'EnableScaling', 'false', allowedValues: ['true','false']
-    
-    MappingParam('InstanceType') do
-      map 'EnvironmentType'
-      attribute 'EcsInstanceType'
-    end
-    MappingParam('AsgMin') do
-      map 'EnvironmentType'
-      attribute 'EcsAsgMin'
-    end
-    MappingParam('AsgMax') do
-      map 'EnvironmentType'
-      attribute 'EcsAsgMax'
-    end
-    MappingParam('KeyName') do
-      map 'AccountId'
-      attribute 'KeyName'
-    end
-    MappingParam('DnsDomain') do
-      map 'AccountId'
-      attribute 'DnsDomain'
-    end
-
-    maximum_availability_zones.times do |az|
-      ComponentParam "SubnetCompute#{az}"
-    end
-
-    ComponentParam 'VPCId', type: 'AWS::EC2::VPC::Id'
-    ComponentParam 'SecurityGroupLoadBalancer', type: 'AWS::EC2::SecurityGroup::Id'
-    ComponentParam 'SecurityGroupBastion', type: 'AWS::EC2::SecurityGroup::Id'
     ComponentParam 'FileSystem' if enable_efs
 
   end
+
+  Component template: 'asg@1.0.0', name: 'asg', render: Inline
+
 end
